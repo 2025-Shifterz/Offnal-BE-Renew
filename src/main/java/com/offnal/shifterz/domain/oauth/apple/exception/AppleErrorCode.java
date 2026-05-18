62f9fa1cfc62b29bac30b63251255c19c6d9b3f8
yunnij <blrdyunnij@gmail.com>
2026-05-18 15:51:18 +0900

    [Refactor] ErrorCode 정리

--- src/main/java/com/offnal/shifterz/domain/oauth/apple/exception/AppleErrorCode.java
+++ src/main/java/com/offnal/shifterz/domain/oauth/apple/exception/AppleErrorCode.java
@@ -14,7 +14,9 @@ public enum AppleErrorCode implements ErrorCode {
     APPLE_PUBLIC_KEY_ERROR("APL003", HttpStatus.INTERNAL_SERVER_ERROR, "Apple 공개키 처리 중 오류가 발생했습니다."),
     APPLE_TOKEN_EXCHANGE_FAIL("APL004", HttpStatus.BAD_GATEWAY, "authorization_code 토큰 교환 실패"),
     APPLE_CLIENT_SECRET_ERROR("APL005", HttpStatus.INTERNAL_SERVER_ERROR, "client secret 생성 실패"),
-    APPLE_REVOKE_FAIL("APL006", HttpStatus.BAD_GATEWAY, "Apple 토큰 revoke 실패");
+    APPLE_REVOKE_FAIL("APL006", HttpStatus.BAD_GATEWAY, "Apple 토큰 revoke 실패"),
+
+    ;
 
     private final String code;
     private final HttpStatus status;
