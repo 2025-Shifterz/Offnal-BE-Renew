654d7c1b7ebfe57119c12e25f403f55431f047f6
yunnij <blrdyunnij@gmail.com>
2026-05-18 15:37:30 +0900

    [Chore] merge 충돌 해결

--- src/main/java/com/offnal/shifterz/domain/oauth/OAuthProviderRegistry.java
+++ src/main/java/com/offnal/shifterz/domain/oauth/OAuthProviderRegistry.java
@@ -2,7 +2,7 @@ package com.offnal.shifterz.domain.oauth;
 
 import com.offnal.shifterz.domain.member.domain.Provider;
 import com.offnal.shifterz.global.exception.CustomException;
-import com.offnal.shifterz.global.exception.ErrorReason;
+import com.offnal.shifterz.global.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -32,7 +32,7 @@ public class OAuthProviderRegistry {
 
     @Getter
     @AllArgsConstructor
-    public enum RegistryErrorCode implements ErrorReason {
+    public enum RegistryErrorCode implements ErrorCode {
         UNSUPPORTED_PROVIDER("AUTH001", HttpStatus.BAD_REQUEST, "지원하지 않는 소셜 로그인 제공자입니다.");
 
         private final String code;
