399413f9057cfa1b1615dae0879907516a49b54a
yunnij <blrdyunnij@gmail.com>
2026-05-28 15:45:25 +0900

    [Chore] 미사용 파일 삭제

--- src/main/java/com/offnal/shifterz/domain/member/service/SocialService.java
+++
@@ -1,7 +0,0 @@
-package com.offnal.shifterz.domain.member.service;
-
-public interface SocialService<T> { // T = 소셜별 DTO
-    String getAccessToken(String code);
-    T getUserInfo(String accessToken);
-}
-
